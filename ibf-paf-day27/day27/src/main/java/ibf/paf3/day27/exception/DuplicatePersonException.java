fd2423f85b79e1309d0b02da8f429b934f28fee6
Jo-cloud85 <joan_tta@live.com.sg>
2024-05-02 11:25:04 +0800

    remove day27

--- ibf-paf-day27/day27/src/main/java/ibf/paf3/day27/exception/DuplicatePersonException.java
+++
@@ -1,7 +0,0 @@
-package ibf.paf3.day27.exception;
-
-public class DuplicatePersonException extends RuntimeException {
-    public DuplicatePersonException(String message) {
-        super(message);
-    }
-}
